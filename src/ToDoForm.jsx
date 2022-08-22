71f76c52fd5d587750979b13acc008f2d23668f5
malo-dev <leadermushio377@gmail.com>
2022-08-22 14:21:48 +0200

    refractors forms-field

--- src/ToDoForm.jsx
+++ src/ToDoForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-const ToDoForm = ({toDoList,settoDoList,editTask}) => {
+const ToDoForm = ({ toDoList, settoDoList, editTask }) => {
 	const [task, settask] = useState("")
 	const [status, setstatus] = useState("")
 	const submitTask = () => {
@@ -11,8 +11,14 @@ const ToDoForm = ({toDoList,settoDoList,editTask}) => {
 	}
   return (
 	<div>
-		  <h4>task</h4>
-		  <h4>statut</h4>
+		  <form  onSubmit={submitTask}>
+			  <input type="text" placeholder='enter task' value={task} onChange={handlechangeInput}/> 
+			  <select name="" value={status} id="" onChange={handleChange}>
+				   <option value="In progress">In progress</option>
+				   <option value="To do">To Do</option>
+				   <option value="Ended">Ended</option>
+			  </select>
+		  </form>
 	</div>
   )
 }
