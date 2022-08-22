36bc18d2af55284b3f8c04f223ca5e5a33ab702b
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-08-22 11:11:11 +0200

    création de la fonction editTask)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import './App.css'
-
+import Header from Header
 function App() {
 
+  const [data,setData]= useState([]);
+  const [editTask,setTask]=useState(null);
 
+  function editedTask(index){
+    return setTask(index)
+  }
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   )
 }
