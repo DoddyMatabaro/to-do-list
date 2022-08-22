81ed6c4fc91fed2d9587fb73ea74753e119f8089
malo-dev <leadermushio377@gmail.com>
2022-08-22 15:49:24 +0200

    Update behavior default of button

--- src/ToDoForm.jsx
+++ src/ToDoForm.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react'
 const ToDoForm = ({ toDoList, settoDoList, editTask }) => {
 	const [task, settask] = useState("")
 	const [status, setstatus] = useState("")
-	const submitTask = () => {
+	const submitTask = (e) => {
+		e.preventDefault()
 		editTask === null ? settoDoList([...toDoList, { task: task, status: status }]) : settoDoList((items) => {
 			return items[editTask]
 		})
